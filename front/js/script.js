371f3c2b999878fb36c6de7d44e9619e290b47ea
wyanda <justwannalearnok@gmail.com>
2022-07-31 17:21:22 -0400

    Added function to load and content products

--- front/js/script.js
+++ front/js/script.js
@@ -6,35 +6,32 @@ fetch('http://localhost:3000/api/products')
 // Get access to the DOM
 const itemsSection = document.getElementById('items');
 
-// The below's function is to     
+// Create elements for the products and set the content
 function displayItems(product) {
     for (let i = 0; i < product.length; i++) {
-        console.log(product[i]);
 
-        let productArray = product[i];
-        console.log(productArray)
+        let productLink = document.createElement('a');
+        let productArticle = document.createElement('article');
+        let productImage = document.createElement('img');
+        let productHeading = document.createElement('h3');
+        let productDescription = document.createElement('p');
+
+        productLink.setAttribute('href', './product.html?id=' + product[i]._id);
+        productImage.setAttribute('alt', product[i].altTxt);
+        productImage.setAttribute('src', product[i].imageUrl);
+        productHeading.textContent = product[i].name;
+        productDescription.textContent = product[i].description;
+        
+        // Append children to the products
+        productArticle.appendChild(productImage);
+        productArticle.appendChild(productHeading);
+        productArticle.appendChild(productDescription);
+        productLink.appendChild(productArticle);
+
+        itemsSection.appendChild(productLink);
     }
+    let productArray = product[i];
+    return productArray;
 }
 
-//Create elements for the products and set their attributes
-function createProducts(product) {
-    let productLink = document.createElement('a');
-    let productArticle = document.createElement('article');
-    let productImage = document.createElement('img');
-    let productHeading = document.createElement('h3');
-    let productDescription = document.createElement('p');
 
-    productLink.setAttribute('');
-    productArticle.setAttribute('');
-    productImage.setAttribute('');
-    productHeading.textContent = product[i].name;
-    productDescription.textContent = '';
-}
-
-// Append children to the products
-productLink.appendChild(productArticle);
-productArticle.appendChild(productImage);
-productArticle.appendChild(productHeading);
-productArticle.appendChild(productDescription);
-
-itemsSection.appendChild(newProductLink);
