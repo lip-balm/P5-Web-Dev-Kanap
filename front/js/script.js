0722b2c26bc6edd8c0892e52e57bc899881410e3
wyanda <justwannalearnok@gmail.com>
2022-07-31 15:02:21 -0400

    Added fetch code

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,40 @@
 // Fetch products from API
-fetch ('http://localhost:3000/api/products')
+fetch('http://localhost:3000/api/products')
     .then(response => response.json())
-    .then(productInfo => displayItems(productInfo));
+    .then(product => displayItems(product));
 
-function displayItems()
+// Get access to the DOM
+const itemsSection = document.getElementById('items');
+
+// The below's function is to     
+function displayItems(product) {
+    for (let i = 0; i < product.length; i++) {
+        console.log(product[i]);
+
+        let productArray = product[i];
+        console.log(productArray)
+    }
+}
+
+//Create elements for the products and set their attributes
+function createProducts(product) {
+    let productLink = document.createElement('a');
+    let productArticle = document.createElement('article');
+    let productImage = document.createElement('img');
+    let productHeading = document.createElement('h3');
+    let productDescription = document.createElement('p');
+
+    productLink.setAttribute('');
+    productArticle.setAttribute('');
+    productImage.setAttribute('');
+    productHeading.textContent = product[i].name;
+    productDescription.textContent = '';
+}
+
+// Append children to the products
+productLink.appendChild(productArticle);
+productArticle.appendChild(productImage);
+productArticle.appendChild(productHeading);
+productArticle.appendChild(productDescription);
+
+itemsSection.appendChild(newProductLink);
