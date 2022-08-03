049790e084f535721947fe67098229a9f3f7c17f
wyanda <justwannalearnok@gmail.com>
2022-08-03 12:18:58 -0400

    Added URLSearchParams

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,8 @@
+// URLSearchParams for API product display
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const pageID = urlParams.get('id');
+
 // Fetch products from API
 fetch('http://localhost:3000/api/products')
     .then(response => response.json())
@@ -5,19 +10,21 @@ fetch('http://localhost:3000/api/products')
 
 // Get access to the DOM
 const itemsSection = document.getElementsByClassName('items');
+const productPageTitle = document.querySelector('title');
+const productTitle = document.getElementById('title');
+const productPrice = document.getElementById('price');
+const productPageDescription = document.getElementById('description');
+const productPageImageDiv = document.getElementsByClassName('item__img')[0];
+const productColors = document.getElementById('colors');
+const productQuantity = document.getElementById('quantity');
 
-// Create product elements, set its contents, and populate on the page
+// original Create product elements, set its contents, and populate on the page
 function contentProductPages(product) {
     for (let i = 0; i < product.length; i++) {
-        const productPageTitle = document.querySelector('title');
-        const productTitle = document.getElementById('title');
-        const productPrice = document.getElementById('price');
-        const productPageDescription = document.getElementById('description');
-
-        const productPageImageDiv = document.getElementsByClassName('item__img');
-
         const productPageImage = document.createElement('img');
-
+        /*var imageDiv = document.createElement('div'); 
+        imageDiv.className = 'imageDiv';*/
+        
         productPageTitle.textContent = product[i].name;
         productPageImage.setAttribute('alt', product[i].altTxt);
         productPageImage.setAttribute('src', product[i].imageUrl);
@@ -25,20 +32,16 @@ function contentProductPages(product) {
         productPrice.textContent = product[i].price;
         productPageDescription.textContent = product[i].description;
 
+        /* imageDiv.appendChild('productPageImageDiv'); */
         productPageImageDiv.appendChild('productPageImage');
-
     }
-    let productPages = product[i];
-    return productPages;
 }
 
 function colorSelection(colors) {
     for(let i=0; i < colors.length; i++) {  
-    const productColors = document.getElementById('colors');
-    const colorSelection = docutment.createElement('option');
-    colorSelection.setAttribute('value', product[i].colors);
-
-
-
+    const colorSelection = document.createElement('option');
+    colorSelection.setAttribute('value', product.colors[i]);
+    colorSelection.textContent = product.colors[i];
+    productColors.appendChild(colorSelection);
     }
 }
