996b77361e52a3e0e015116bdf48ae6c14c4fc4c
wyanda <justwannalearnok@gmail.com>
2022-08-04 19:41:36 -0400

    Added function for product page content/colors

--- front/js/product.js
+++ front/js/product.js
@@ -2,6 +2,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const pageID = urlParams.get('id');
+console.log(pageID);
 
 // Fetch products from API
 fetch('http://localhost:3000/api/products')
@@ -18,30 +19,32 @@ const productPageImageDiv = document.getElementsByClassName('item__img')[0];
 const productColors = document.getElementById('colors');
 const productQuantity = document.getElementById('quantity');
 
-// original Create product elements, set its contents, and populate on the page
+// Content the each product page with respective information
 function contentProductPages(product) {
     for (let i = 0; i < product.length; i++) {
-        const productPageImage = document.createElement('img');
-        /*var imageDiv = document.createElement('div'); 
-        imageDiv.className = 'imageDiv';*/
-        
-        productPageTitle.textContent = product[i].name;
-        productPageImage.setAttribute('alt', product[i].altTxt);
-        productPageImage.setAttribute('src', product[i].imageUrl);
-        productTitle.textContent = product[i].name;
-        productPrice.textContent = product[i].price;
-        productPageDescription.textContent = product[i].description;
+        if (product[i]._id === pageID) {
+            let productPageImage = document.createElement('img');
+            productPageImage.setAttribute('alt', product[i].altTxt);
+            productPageImage.setAttribute('src', product[i].imageUrl);
+            productPageImageDiv.appendChild(productPageImage);
 
-        /* imageDiv.appendChild('productPageImageDiv'); */
-        productPageImageDiv.appendChild('productPageImage');
+            productPageTitle.textContent = product[i].name;
+            productTitle.textContent = product[i].name;
+            productPrice.textContent = product[i].price;
+            productPageDescription.textContent = product[i].description;
+
+            colorSelection(product[i].colors);
+        }
     }
 }
 
+// Function for populating the color dropdown
 function colorSelection(colors) {
+    console.log(colors);
     for(let i=0; i < colors.length; i++) {  
     const colorSelection = document.createElement('option');
-    colorSelection.setAttribute('value', product.colors[i]);
-    colorSelection.textContent = product.colors[i];
+    colorSelection.setAttribute('value', colors[i]);
+    colorSelection.textContent = colors[i];
     productColors.appendChild(colorSelection);
     }
 }
